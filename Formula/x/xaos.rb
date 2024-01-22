ae9ba226152a75e403d31dc4e79e2f03e44be616
Zoltán Kovács <zoltan@geogebra.org>
2024-01-22 12:15:57 +0100

    Fix license

--- Formula/x/xaos.rb
+++ Formula/x/xaos.rb
@@ -3,7 +3,7 @@ class Xaos < Formula
   homepage "https://xaos-project.github.io/"
   url "https://github.com/xaos-project/XaoS/archive/refs/tags/4.3.2-cmake2.tar.gz"
   sha256 "1abb14b4ccb2cf12d8ff3bac787b1bc4f956bf15419d2fdcdc466ffe3e45ce65"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
